bee0ed730dd0ae37c1a28b4bf42fdb92cbff7281
Geovanna Nascimento <73962068+geordtl@users.noreply.github.com>
2025-07-14 18:42:38 -0400

    :sparkles: Feat: implementando mobile first

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
+gsap.registerPlugin(SplitText,TextPlugin)
 
 window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 400);
-        });
 
-        setTimeout(() => {
-            logoSpan.forEach((span, idx) => {
-                span.classList.remove('active');
-                span.classList.add('fade');
-            });
-        }, 2000);
+    // setTimeout(() => {
+    //     logoSpan.forEach((span, idx) => {
+    //         setTimeout(() => {
+    //             span.classList.add('active');
+    //         }, (idx + 1) * 400);
+    //     });
 
-        setTimeout(() => {
-            intro.style.top = '-100vh';
-        }, 2300);
+    //     setTimeout(() => {
+    //         logoSpan.forEach((span, idx) => {
+    //             span.classList.remove('active');
+    //             span.classList.add('fade');
+    //         });
+    //     }, 2000);
+
+    //     setTimeout(() => {
+    //         intro.style.top = '-100vh';
+    //         logoSpan.classList.add('hidden');
+    //     }, 2300);
     });
-})
+// })
+
+document.fonts.ready.then(() => {
+  gsap.set("#textos", { opacity: 1 });
+  let split = SplitText.create(".animate-me", { type: "words", aria: "hidden" });
+
+  gsap.from(split.words, {
+    opacity: 0,
+    duration: 2,
+    ease: "sine.out",
+    stagger: 0.1,
+  });
+});
